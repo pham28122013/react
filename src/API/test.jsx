8a1c2a45302df92e6cb7bae11dcd944e8b32e2da
pham28122013 <15i1nguyendipham@gmail.com>
2020-04-28 16:51:01 +0700

    [Add] test_api

---
+++ src/API/test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.scss';
+
+function Test() {
+  return (
+    <div className="test">
+      <header className="test-header">
+        <p>
+         nguyen di pham
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Test;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Test from './API/test';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Test/>
   </React.StrictMode>,
   document.getElementById('root')
 );
